379331017b44d5befc13e2da19b56c39fa07e2ab
Chidinma21 <chidinmaonuora1@gmail.com>
2022-03-18 20:19:33 +0100

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,25 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import "./style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red">Your name here</h1>
+
+        <br></br>
+        <img src={require("./imageInSrc.jpeg")} alt="in src"></img>
+
+        <br></br>
+        <img src="../imageInPublic.png" alt="in public"></img>
+      </div>
+
+      <video width="320" height="240" controls>
+        <source src="myVideo.mp4" type="video/mp4"></source>
+      </video>
+    </div>
+  );
+}
+
+export default App;
