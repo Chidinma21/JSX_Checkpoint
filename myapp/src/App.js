0b8049707ca26e7d12d9b19cecaf80f6c668f449
Chidinma21 <chidinmaonuora1@gmail.com>
2022-03-20 19:49:02 +0100

    corrections

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -8,14 +8,14 @@ function App() {
       <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
         <h1 className="title red">Your name here</h1>
 
-        <br></br>
+        <br />
         <img src={require("./imageInSrc.jpeg")} alt="in src"></img>
 
-        <br></br>
+        <br />
         <img src="../imageInPublic.png" alt="in public"></img>
       </div>
 
-      <video width="320" height="240" controls>
+      <video style={{ width: 320, height: 240 }} controls>
         <source src="myVideo.mp4" type="video/mp4"></source>
       </video>
     </div>
